f6da7168b92b01df9b34ba4bb6228c5f463c09b8
karankumarshreds <naileditnerd@gmail.com>
2021-08-29 14:14:10 +0530

    add failing teste

--- main_test.go
+++ main_test.go
@@ -3,7 +3,10 @@ package main_test
 import (
 	"testing"
 	"log"
+	"fmt"
 	"os"
+	"net/http"
+	"net/http/httptest"
 	"github.com/karankumarshreds/GoPostgresAPI.git"
 )
 
@@ -12,12 +15,46 @@ var a main.App
 
 // this will be executed before each tests 
 func TestMain(m *testing.M) {
+	fmt.Println("Test main function")
 	a.Initialize(
 		os.Getenv("DB_USERNAME"),
 		os.Getenv("DB_PASSWORD"),
 		os.Getenv("DB_NAME"),
 	)
+
+	ensureTableExists()
+	
+	code := m.Run()
+
+	os.Exit(code)
 }
+
+
+
+// T is a type passed to Test functions to manage test state and support formatted test logs.
+func TestEmptyTable (t *testing.T) {
+	clearTable()
+	req, _ := http.NewRequest("GET", "/products", nil)
+	// response := executeRequest(req)
+	// rw here is the "rr" (response recorder) to record the response 
+	rw := httptest.NewRecorder()
+	a.Router.ServeHTTP(rw, req)	
+	// checking the response status code 
+	if http.StatusOK != rw.Code {
+		// this method ends the running tests 
+		t.Errorf("Expected response %v got %v", http.StatusOK, rw.Code)
+	}
+	// checking the body response
+	body := rw.Body.String();
+	if body != "[]" {
+		t.Errorf("Expected an empty array. Got %v", body)
+	}
+
+}	
+
+// *************************** // 
+// HELPER FUNCTIONS 
+// *************************** // 
 
 // makes sure the table exists by creating one 
 func ensureTableExists() {
@@ -39,3 +76,11 @@ func clearTable() {
 	a.DB.Exec("DELETE FROM products")
 	a.DB.Exec("ALTER SEQUENCE products_id_seq RESTART WITH 1")
 }
+
+// The httptest.ResponseRecorder is an implementation of http.ResponseWriter
+// As "rw http.ResponseWriter" assemnles the HTTP server's response by writing to it 
+// which we send back the to the HTTP CLient. Similarly, httptest.ResponseRecorder 
+// is used to record the response that the handler will write for the client's request 
+// func executeRequest(req *http.Request) *httptest.ResponseRecorder {
+// 	httptest.NewRecoer
+// }
