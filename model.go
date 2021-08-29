791b10966bea2f035655f576c38c034513739da3
karankumarshreds <naileditnerd@gmail.com>
2021-08-29 20:10:56 +0530

    add controllers

--- model.go
+++ model.go
@@ -12,19 +12,36 @@ type Product struct {
 }
 
 func (p *Product) getProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	return db.QueryRow(
+		"SELECT name, price FROM products WHERE id =$1", 
+		p.ID,
+	).Scan(&p.Name, &p.Price) // scan method only works on method that return row(s)
 }
 
 func (p *Product) updateProduct(db *sql.DB) error {
-	return errors.New("Not implemented")
+	_, err := db.Exec(
+		"UPDATE products SET name=$1, price=$2 WHERE id=$3",
+		p.Name, 
+		p.Price,
+		p.ID,
+	)
+	return err
 }
 
 func (p *Product) deleteProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+	_, err := db.Exec(
+		"DELETE FROM products WHERE id=$1",
+		p.ID,
+	)
+	return err
 }
 
 func (p *Product) createProduct(db *sql.DB) error {
-  return errors.New("Not implemented")
+  return db.QueryRow(
+		"INSERT INTO products(name, price) VALUES($1, $2) RETURNING name, price, id",
+		p.Name, 
+		p.Price,
+	).Scan(&p.Name, &p.Price, &p.ID)
 }
 
 func getProducts(db *sql.DB, start, count int) ([]Product, error) {
@@ -48,9 +65,9 @@ func getProducts(db *sql.DB, start, count int) ([]Product, error) {
 		err := rows.Scan(&p.ID, &p.Name, &p.Price)
 		if err != nil {
 			return nil, err
-		} else {
-			products = append(products, p)
-		}
+		} 
+		products = append(products, p)
+		
 	}
 	return products, nil
 }
